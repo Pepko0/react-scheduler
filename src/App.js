371a410148dc056b6b11974670bf75fe78b38162
Kacper Lewko <k.lewkosga@gmail.com>
2024-08-18 23:34:31 +0200

    First version Scheduler

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Scheduler from './components/Scheduler';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  { start_date: '2022-11-15 6:00', end_date: '2022-11-15 8:00', text:'Event 1', id: 1},
+  { start_date: '2022-11-15 10:00', end_date: '2022-11-15 18:00', text:'Event 2', id: 2}
+]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className='scheduler-container'>
+          <Scheduler events={data}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
